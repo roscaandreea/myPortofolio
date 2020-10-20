907e2a8ad9fc407d45a48c5d48cd9cef8802739f
roscaandreea <andreea.rosca@info.uaic.ro>
2020-10-20 15:52:06 +0300

    added first commit with logo and navBar component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+//import components
+import NavBar from './components/NavBar/navBar.component';
+const  App = () => {
+  return (
+     <div>
+       <NavBar />
+      Here is my portofolio stuff
+     </div>
+  );
+};
+
+export default App;
