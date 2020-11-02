1ecae59b81d42db970c30ad014222b7864ecdc6a
roscaandreea <andreea.rosca@info.uaic.ro>
2020-11-02 15:56:42 +0200

    Updates

---
+++ precache-manifest.fb4e759a5afa0b8ddc38259f54073f4a.js
@@ -0,0 +1,202 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a28da091609267242c26ca66a40fdc6",
+    "url": "/myPortofolio/index.html"
+  },
+  {
+    "revision": "27a44436e58305555723",
+    "url": "/myPortofolio/static/css/main.a77c89d5.chunk.css"
+  },
+  {
+    "revision": "6da6126c502991386a73",
+    "url": "/myPortofolio/static/js/2.56962782.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/myPortofolio/static/js/2.56962782.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27a44436e58305555723",
+    "url": "/myPortofolio/static/js/main.e910b0d3.chunk.js"
+  },
+  {
+    "revision": "c8fe4c81453ed84817d6",
+    "url": "/myPortofolio/static/js/runtime-main.f51fcd5b.js"
+  },
+  {
+    "revision": "c0ace61758da61f6ce89c0568d0e6540",
+    "url": "/myPortofolio/static/media/CE.c0ace617.webp"
+  },
+  {
+    "revision": "933a981e4ab760d41e7adabf394d4cf0",
+    "url": "/myPortofolio/static/media/D3.933a981e.png"
+  },
+  {
+    "revision": "874254c3bca46e5352a9c5076d8dac54",
+    "url": "/myPortofolio/static/media/DS.874254c3.webp"
+  },
+  {
+    "revision": "5bf8a3ca0bb6bfb41063cdd672be9ad9",
+    "url": "/myPortofolio/static/media/GraphQL.5bf8a3ca.svg"
+  },
+  {
+    "revision": "45ff4c29181bf8b649a4f157040ec99b",
+    "url": "/myPortofolio/static/media/ML.45ff4c29.webp"
+  },
+  {
+    "revision": "bf989778565c49da91f9e0ef13dbdc5c",
+    "url": "/myPortofolio/static/media/background.bf989778.webp"
+  },
+  {
+    "revision": "ffded2381d25dd906406d6e07f91dea5",
+    "url": "/myPortofolio/static/media/bootstrap-4.ffded238.svg"
+  },
+  {
+    "revision": "3707da3c6a394c7ee2523273f8d98a25",
+    "url": "/myPortofolio/static/media/css3.3707da3c.svg"
+  },
+  {
+    "revision": "bcf22073c9f13bab1761b90bce9fec55",
+    "url": "/myPortofolio/static/media/django.bcf22073.svg"
+  },
+  {
+    "revision": "16562ba390c108d482e2339f87d7ce15",
+    "url": "/myPortofolio/static/media/eventBooking.16562ba3.webp"
+  },
+  {
+    "revision": "c6bab64b183f96aa47077fd3db37afd0",
+    "url": "/myPortofolio/static/media/express.c6bab64b.svg"
+  },
+  {
+    "revision": "d98463abd102acdc2b1eb8d5eb70b2d2",
+    "url": "/myPortofolio/static/media/flask.d98463ab.svg"
+  },
+  {
+    "revision": "1190a19ce3cc940741a60137a1e074cb",
+    "url": "/myPortofolio/static/media/git-icon.1190a19c.svg"
+  },
+  {
+    "revision": "0ff1a6f08182be4aa63a0b54da1b9e57",
+    "url": "/myPortofolio/static/media/github.0ff1a6f0.svg"
+  },
+  {
+    "revision": "676e8188f003ed5d4882a0d0f378dad3",
+    "url": "/myPortofolio/static/media/html-5.676e8188.svg"
+  },
+  {
+    "revision": "13a096531e50277a3fb17460c39db221",
+    "url": "/myPortofolio/static/media/java.13a09653.svg"
+  },
+  {
+    "revision": "2d2e31cc63a96c7e6044f52d30fd21b9",
+    "url": "/myPortofolio/static/media/javascript.2d2e31cc.svg"
+  },
+  {
+    "revision": "51740791fbc2ef604e33af19e8287303",
+    "url": "/myPortofolio/static/media/logo.51740791.webp"
+  },
+  {
+    "revision": "e6f67a2074fb02937303e588024fbf89",
+    "url": "/myPortofolio/static/media/matplotlib.e6f67a20.svg"
+  },
+  {
+    "revision": "8fefae37a1730abf2bff10f388fed3be",
+    "url": "/myPortofolio/static/media/microsoft_.NET.8fefae37.svg"
+  },
+  {
+    "revision": "2985235df31b8742d893d5b7e80145c5",
+    "url": "/myPortofolio/static/media/mongodb.2985235d.svg"
+  },
+  {
+    "revision": "54d7bddcd3019ec3c3b23db00160ccef",
+    "url": "/myPortofolio/static/media/mssql.54d7bddc.svg"
+  },
+  {
+    "revision": "a123152868245dee71c441124a98ac3e",
+    "url": "/myPortofolio/static/media/nodejs.a1231528.svg"
+  },
+  {
+    "revision": "c52024c37a7fdda6eb29a058ca2663bf",
+    "url": "/myPortofolio/static/media/pandas.c52024c3.svg"
+  },
+  {
+    "revision": "bffcff3d1af2f15e7071f83d4d81f5ea",
+    "url": "/myPortofolio/static/media/plotly.bffcff3d.svg"
+  },
+  {
+    "revision": "d7c985e7cca077dc2d77f89a66573e35",
+    "url": "/myPortofolio/static/media/postgresql.d7c985e7.svg"
+  },
+  {
+    "revision": "13b6d0df44ccc57b0f74a1124a563321",
+    "url": "/myPortofolio/static/media/profile.13b6d0df.webp"
+  },
+  {
+    "revision": "146716bdf1a351db5705ec1e6a25ed08",
+    "url": "/myPortofolio/static/media/python.146716bd.svg"
+  },
+  {
+    "revision": "83fe61b05935f090b671b08aeb77b63d",
+    "url": "/myPortofolio/static/media/react-bootstrap.83fe61b0.svg"
+  },
+  {
+    "revision": "024196716802e272adb3cbefb81fd557",
+    "url": "/myPortofolio/static/media/react-router.02419671.svg"
+  },
+  {
+    "revision": "4009eef3b3e291f296e71962c70895c5",
+    "url": "/myPortofolio/static/media/react.4009eef3.svg"
+  },
+  {
+    "revision": "d67311e84ba2e9dc19b6b7f6ae52c183",
+    "url": "/myPortofolio/static/media/recipeApp.d67311e8.webp"
+  },
+  {
+    "revision": "aa0a337603611296524a666b62a9ff23",
+    "url": "/myPortofolio/static/media/redux.aa0a3376.svg"
+  },
+  {
+    "revision": "b446ca5a397c1ef6fb94cd02c4e94335",
+    "url": "/myPortofolio/static/media/sass-1.b446ca5a.svg"
+  },
+  {
+    "revision": "d724714f9609b67d470a1b0986e9db57",
+    "url": "/myPortofolio/static/media/scikit_learn.d724714f.svg"
+  },
+  {
+    "revision": "8bce7aef886c3872c218e0e6ef8bdccb",
+    "url": "/myPortofolio/static/media/slide1.8bce7aef.webp"
+  },
+  {
+    "revision": "7256f309520276be70acd97720610bc6",
+    "url": "/myPortofolio/static/media/slide2.7256f309.webp"
+  },
+  {
+    "revision": "8e5b87b60151049a8df9ec0bd30978f0",
+    "url": "/myPortofolio/static/media/slide3.8e5b87b6.webp"
+  },
+  {
+    "revision": "e79a42b9281a3b30f1302d5452f9884f",
+    "url": "/myPortofolio/static/media/styled-components.e79a42b9.svg"
+  },
+  {
+    "revision": "6c3e716c90caacfb47fa7bf9fb7a443f",
+    "url": "/myPortofolio/static/media/tempApp.6c3e716c.webp"
+  },
+  {
+    "revision": "851a1181b2fd0ec1446b5342279cbafe",
+    "url": "/myPortofolio/static/media/toDoApp.851a1181.webp"
+  },
+  {
+    "revision": "3de182d2120823fe49c27eeb1765c53a",
+    "url": "/myPortofolio/static/media/typescript.3de182d2.svg"
+  },
+  {
+    "revision": "f175bad3ca7235c70645bf31b7d8b411",
+    "url": "/myPortofolio/static/media/uaic.f175bad3.svg"
+  },
+  {
+    "revision": "6594d7186356ac2ec4b69cc91d86db18",
+    "url": "/myPortofolio/static/media/watercolor4.6594d718.webp"
+  }
+]);
