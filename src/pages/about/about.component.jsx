4184b2032d9d6f516f0986eb98b62c544601e509
roscaandreea <andreea.rosca@info.uaic.ro>
2021-08-09 17:24:40 +0300

    Deleted thumbnail-img

--- src/pages/about/about.component.jsx
+++ src/pages/about/about.component.jsx
@@ -17,7 +17,7 @@ const About = () => {
                         {/* Profile picture */}
                         <Col xs={12} md={6}>
                             <Row className="justify-content-center mb-2 mr-2 ">
-                                <Image className="profile justify-content-end shadow" alt="profile" src={Profile} />
+                                <Image className="profile justify-content-end shadow rounded" alt="profile" src={Profile} />
                             </Row>
                         </Col>
                         {/* About description + social link */}
