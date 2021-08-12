5eaa5518ad9ec667185cfee03da2f6641ab88073
roscaandreea <andreea.rosca@info.uaic.ro>
2021-08-12 19:07:07 +0300

    Added active events for service worker

--- src/sw.js
+++ src/sw.js
@@ -1,1 +1,7 @@
-console.log("Hello World");
+workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
+self.addEventListener('install', event => {
+    console.log('install')
+  })
+  self.addEventListener('activate', event => {
+    console.log('activate')
+  })
